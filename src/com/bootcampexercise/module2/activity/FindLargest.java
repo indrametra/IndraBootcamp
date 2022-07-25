97bef460b7b84024623240ed10e29bf3882f9a9e
Indra Metra <indrametra@gmail.com>
2022-07-25 17:24:13 +0300

    lielakais

---
+++ src/com/bootcampexercise/module2/activity/FindLargest.java
@@ -0,0 +1,16 @@
+package com.bootcampexercise.module2.activity;
+
+public class FindLargest {
+    public static void main(String args[]) {
+        int d = 5;
+        double z = 5.5;
+
+        if (d >  z) {
+            System.out.println("d is largest, because "+d+" > than "+z);
+        } else if (z > d) {
+            System.out.println("z is largest, because "+z+" > than "+d);
+        }
+
+
+    }
+}
