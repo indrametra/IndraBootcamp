102acf7311011257ac6a2cc5b297410a32dc21fd
Indra Metra <indrametra@gmail.com>
2022-07-25 16:38:32 +0300

    un fails

---
+++ src/com/bootcampexercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,13 @@
+package com.bootcampexercise.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String []args) {
+        int z=9;
+        int j = z+2;
+        int sum = z + j;
+        System.out.println("Sum of z and j is "+z+j);
+        System.out.println("Sum of i and j is " + (z + j));
+        System.out.println("Sum of i and j is " + sum);
+
+    }
+}
