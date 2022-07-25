ac6bbe4fa7844ea2585fd45b05fcbd1732ea5532
Indra Metra <indrametra@gmail.com>
2022-07-25 17:44:17 +0300

    skaitlu vardi

---
+++ src/com/bootcampexercise/module2/activity/NumToWords.java
@@ -0,0 +1,53 @@
+package com.bootcampexercise.module2.activity;
+
+public class NumToWords {
+    public static void main(String[] args) { //activity 4 switch case
+
+        int x = 5;
+        switch (x) {
+            case 1:
+                System.out.println("The number is one");
+                break;
+
+            case 2:
+                System.out.println("The number is two");
+                break;
+
+            case 3:
+                System.out.println("The number is three");
+                break;
+
+            case 4:
+                System.out.println("The number is four");
+                break;
+
+            case 5:
+                System.out.println("The number is five");
+                break;
+
+            case 6:
+                System.out.println("The number is six");
+                break;
+
+            case 7:
+                System.out.println("The number is seven");
+                break;
+
+            case 8:
+                System.out.println("The number is eight");
+                break;
+
+            case 9:
+                System.out.println("The number is nine");
+                break;
+
+            case 10:
+                System.out.println("The number is ten");
+                break;
+
+            default:
+                System.out.println("The number is out of range");
+        }
+
+    }
+}
