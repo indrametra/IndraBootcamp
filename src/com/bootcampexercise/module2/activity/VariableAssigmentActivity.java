aeae4e91bf05675b1200d2255a8b616caa04c01a
Indra Metra <indrametra@gmail.com>
2022-07-25 15:09:27 +0300

    java fails

--- src/com/bootcampexercise/module2/activity/VariableAssigmentActivity.java
+++ src/com/bootcampexercise/module2/activity/VariableAssigmentActivity.java
@@ -2,4 +2,10 @@ package com.bootcampexercise.module2.activity;
 
 public class VariableAssigmentActivity {
 
-}
+         public static void main(String[] args) {
+            int j = 5;
+             System.out.println("first value"+j);
+            j = 12;
+             System.out.println("second value" +j);
+        }
+    }
