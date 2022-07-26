004108ef14921f4a3c1eb96233b75c98d1a6672e
Indra Metra <indrametra@gmail.com>
2022-07-26 10:33:24 +0300

    array1

---
+++ src/com/bootcampexercise/module2/sample/ArrayExample.java
@@ -0,0 +1,11 @@
+package com.bootcampexercise.module2.sample;
+
+public class ArrayExample {
+    public static void main(String[] args){
+        String [] stringAray = {"Hello", "How", "Are", "You"};
+        System.out.println("0th index pf array has"+stringAray[0]+" "+stringAray[1]+" "+stringAray[2]+" "+stringAray[3]+" ");
+        stringAray[0]="Hi";
+        System.out.println("0th index pf array has "+stringAray[0]);
+    }
+
+}
