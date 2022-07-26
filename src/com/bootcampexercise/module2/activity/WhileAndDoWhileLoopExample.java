6c30a77c6087be89e55ed7ab043795e5960d8124
Indra Metra <indrametra@gmail.com>
2022-07-26 10:02:03 +0300

    while loop

---
+++ src/com/bootcampexercise/module2/activity/WhileAndDoWhileLoopExample.java
@@ -0,0 +1,17 @@
+package com.bootcampexercise.module2.activity;
+
+public class WhileAndDoWhileLoopExample {
+     public static void main(String[] args) {
+
+            // Print all even numbers less than 100
+            int i = 1;
+            while (i < 100) {
+                System.out.println(i);
+                i = i + 2;
+
+
+            //TODO: write code to Print all odd numbers less than 100
+        }
+
+    }
+}
---
+++ src/com/bootcampexercise/module2/sample/PrintNumWithWhile.java
@@ -0,0 +1,13 @@
+package com.bootcampexercise.module2.sample;
+
+public class PrintNumWithWhile {
+    public static void main(String args[]){
+        int counter = 1;
+        while (counter<11)
+         {
+            System.out.println("The value of x is: "+counter);
+            counter++;
+        }
+
+    }
+}
