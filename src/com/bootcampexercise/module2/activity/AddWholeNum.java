e312d46a288b700445651fb1b16de86cb21c4e52
Indra Metra <indrametra@gmail.com>
2022-07-26 09:20:01 +0300

    for loop

---
+++ src/com/bootcampexercise/module2/activity/AddWholeNum.java
@@ -0,0 +1,12 @@
+package com.bootcampexercise.module2.activity;
+
+public class AddWholeNum {
+    public static void main(String args[])
+    {
+        int sum=0;
+        for (int i = 50; i<=100; i++){
+            sum = sum +i;
+        }
+        System.out.println("Sum of first 10 numbers is " + sum);
+    }
+}
